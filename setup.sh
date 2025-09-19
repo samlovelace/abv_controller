d491c8af99743e5d310e671a72e0797bddbc52c3
samlovelace <samlovelace12@gmail.com>
2025-09-18 22:12:38 -0400

    removes duplicate code from setup script

--- setup.sh
+++ setup.sh
@@ -1,52 +1,5 @@
 #!/bin/bash
 
-install_ros() {
-    echo "[ROS] Starting installation of ROS 2 Humble..."
-
-    # --- Configure UTF-8 Locale ---
-    if ! locale | grep -q "UTF-8"; then
-        echo "[ROS] Configuring UTF-8 locale..."
-        apt install -y locales
-        locale-gen en_US en_US.UTF-8
-        update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-        export LANG=en_US.UTF-8
-    fi
-
-    # --- Set Timezone Non-Interactively ---
-    echo "[ROS] Setting timezone to America/New_York..."
-    ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-    DEBIAN_FRONTEND=noninteractive apt install -y tzdata
-    dpkg-reconfigure -f noninteractive tzdata
-
-    # --- Add Required Tools ---
-    apt install -y software-properties-common curl gnupg lsb-release
-
-    # --- Add Universe Repo ---
-    add-apt-repository universe
-
-    # --- Add ROS 2 GPG Key ---
-    curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key \
-        -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-    # --- Add ROS 2 Repository ---
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] \
-        http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" \
-        > /etc/apt/sources.list.d/ros2.list
-
-    # --- Install ROS 2 Humble and Tools ---
-    apt update
-    apt install -y ros-humble-desktop \
-                   python3-colcon-common-extensions \
-                   python3-rosdep \
-                   python3-vcstool
-
-    # --- Source ROS Environment ---
-    echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
-    source /opt/ros/humble/setup.bash
-
-    echo "[✔] ROS 2 Humble installed and environment sourced."
-}
-
 WS=$1
 
 # Subtract 2 from total cores
@@ -82,9 +35,7 @@ for pkg in "${DEPENDENCIES[@]}"; do
 done
 
 # Ensure the directory exists
-#mkdir -p "$WS"/src
-
-#git clone https://github.com/samlovelace/robot_idl.git "$WS/src/robot_idl"
+mkdir -p "$WS"/src
 
 cd "$WS"
 pwd
