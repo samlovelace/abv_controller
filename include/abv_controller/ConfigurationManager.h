5c227150f24d4c25bee6c5e46e281b9859bce511
samlovelace <samlovelace12@gmail.com>
2025-10-03 21:38:10 -0400

    adds getter for vehicle config

--- include/abv_controller/ConfigurationManager.h
+++ include/abv_controller/ConfigurationManager.h
@@ -20,6 +20,7 @@ public:
     StateTrackerConfig getStateTrackerConfig() {return mConfigurations.vehicleConfig.stateTrackerConfig;}
     StatePublisherConfig getStatePublisherConfig() {return mConfigurations.vehicleConfig.statePublisherConfig;}
     ControllerConfig getControllerConfig() {return mConfigurations.vehicleConfig.controllerConfig;}
+    VehicleConfig getVehicleConfig() {return mConfigurations.vehicleConfig; }
 
 private: 
     ConfigurationManager();
